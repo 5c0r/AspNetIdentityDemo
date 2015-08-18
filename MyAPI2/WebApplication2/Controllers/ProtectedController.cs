c2bb098d8e83b54d14a281cf514c26b441ed63fb
nmt1994 <nmt1994@gmail.com>
2015-08-18 12:10:30 +0700

    Initial commit

---
+++ MyAPI2/WebApplication2/Controllers/ProtectedController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Web.Http;
+using WebApplication2.Infrastructure;
+
+namespace WebApplication2.Controllers
+{
+    [RoutePrefix("api/protected")]
+    public class ProtectedController : ApiController
+    {
+        [Route("")]
+        [ClaimsAuthorization(ClaimType = "Test", ClaimValue = "False")]
+        public IEnumerable<object> Get()
+        {
+            var identity = User.Identity as ClaimsIdentity;
+
+            return identity.Claims.Select(c => new
+            {
+                Type = c.Type,
+                Value = c.Value
+            });
+        }
+    }
+}
